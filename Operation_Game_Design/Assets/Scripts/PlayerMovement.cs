d56760f705d419c6b6e1b64593c6762e5a94b079
jaisaka <jaitheboss1@gmail.com>
2017-10-24 15:07:16 -0400

    guy moves

---
+++ Operation_Game_Design/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	float movementSpeed, xTrans, yTrans;
+	Vector2 pos;
+	// Use this for initialization
+	void Start () {
+		movementSpeed = .025f;
+		pos = new Vector2 (0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos = transform.position;
+		xTrans = Input.GetAxis ("Horizontal") * movementSpeed;
+		yTrans = Input.GetAxis ("Vertical") * movementSpeed;
+		pos = new Vector2 (pos.x + xTrans, pos.y + yTrans);
+		if (pos.y > Camera.main.orthographicSize) {
+			pos.y = -Camera.main.orthographicSize;
+		}
+		if (pos.y < -Camera.main.orthographicSize) {
+			pos.y = Camera.main.orthographicSize;
+		}
+		float screenRatio = (float)Screen.width / (float)Screen.height;
+		float orthoWidth = screenRatio * Camera.main.orthographicSize;
+		if (pos.x > orthoWidth) {
+			pos.x = -orthoWidth;
+		}
+		if (pos.x < -orthoWidth) {
+			pos.x = orthoWidth;
+		}
+		transform.position = pos;
+	}
+}
