e714eec748e106c1224822bbbe24adb364c095f9
jaisaka <jaitheboss1@gmail.com>
2017-10-31 10:37:55 -0400

    player now looks at mouse pos

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+	[SerializeField]
 	float movementSpeed, xTrans, yTrans;
+	Quaternion rotation;
+	bool running;
 	Vector2 pos;
+	Vector3 mousePos;
 	// Use this for initialization
 	void Start () {
-		movementSpeed = .025f;
+		movementSpeed = .05f;
 		pos = new Vector2 (0,0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		if (Input.mousePresent) {
+			mousePos = Input.mousePosition;
+			mousePos = Camera.main.ScreenToWorldPoint (mousePos);
+			rotation = Quaternion.LookRotation(transform.position - mousePos, Vector3.forward);
+			transform.rotation = rotation;
+			transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		}
 		pos = transform.position;
 		xTrans = Input.GetAxis ("Horizontal") * movementSpeed;
 		yTrans = Input.GetAxis ("Vertical") * movementSpeed;
@@ -32,5 +43,15 @@ public class PlayerMovement : MonoBehaviour {
 			pos.x = orthoWidth;
 		}
 		transform.position = pos;
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			movementSpeed = .1f;
+			running = true;
+		}
+		if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			running = false;
+		}
+		if (!running) {
+			movementSpeed = .05f;
+		}
 	}
 }
