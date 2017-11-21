cb4eba97fab489c1a1161bff78320db31e1259d1
jaisaka <jaitheboss1@gmail.com>
2017-11-21 15:33:22 -0500

    player shoots now, yay

--- Assets/Scripts/Player Scripts/PlayerShoot.cs
+++ Assets/Scripts/Player Scripts/PlayerShoot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour {
-
+	public GameObject bullet;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,13 @@ public class PlayerShoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKey(KeyCode.Space))
+		{
+			var attackInst = Instantiate (bullet);
+			attackInst.transform.SetParent (gameObject.transform);
+			attackInst.transform.localPosition = new Vector3 (0, 0, 0);
+			attackInst.transform.localRotation = Quaternion.Euler (0, 0, 0);
+			attackInst.transform.SetParent (null);
+		}
 	}
 }
