5a3983705f5404570bd04232162bf07fffe456c5
PotatoOX <mr.potatoscience@gmail.com>
2017-11-28 15:30:22 -0500

    Jai is a stoppid nooger(also i cleaned up repeated code)

--- Assets/Scripts/Player Scripts/PlayerShoot.cs
+++ Assets/Scripts/Player Scripts/PlayerShoot.cs
@@ -3,9 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
+
+
 public class PlayerShoot : MonoBehaviour {
 	public GameObject bullet;
 	public int fireModeIndex;
+
+	void InstantiateBullet () {
+		var attackInst = Instantiate (bullet);
+		attackInst.transform.SetParent (gameObject.transform);
+		attackInst.transform.localPosition = new Vector3 (0, 0, 0);
+		attackInst.transform.localRotation = Quaternion.Euler (0, 0, 0);
+		attackInst.transform.SetParent (null);
+	}
+
 	// Use this for initialization
 	void Start () {
 		fireModeIndex = 0;
@@ -22,27 +36,16 @@ public class PlayerShoot : MonoBehaviour {
 			}
 		}
 		if (Input.GetKeyDown (KeyCode.Space) && fireModeIndex == 0) {
-			var attackInst = Instantiate (bullet);
-			attackInst.transform.SetParent (gameObject.transform);
-			attackInst.transform.localPosition = new Vector3 (0, 0, 0);
-			attackInst.transform.localRotation = Quaternion.Euler (0, 0, 0);
-			attackInst.transform.SetParent (null);
+			InstantiateBullet ();
 		}
 		if (Input.GetKey (KeyCode.Space) && fireModeIndex == 1) {
-			var attackInst = Instantiate (bullet);
-			attackInst.transform.SetParent (gameObject.transform);
-			attackInst.transform.localPosition = new Vector3 (0, 0, 0);
-			attackInst.transform.localRotation = Quaternion.Euler (0, 0, 0);
-			attackInst.transform.SetParent (null);	
+			InstantiateBullet ();
 		}
 		if (Input.GetKeyDown (KeyCode.Space) && fireModeIndex == 2) {
 			for (int i = 0; i < 3; i++) {
-				var attackInst = Instantiate (bullet);
-				attackInst.transform.SetParent (gameObject.transform);
-				attackInst.transform.localPosition = new Vector3 (0, 0, 0);
-				attackInst.transform.localRotation = Quaternion.Euler (0, 0, 0);
-				attackInst.transform.SetParent (null);
+				InstantiateBullet ();
 				Wait ();
+
 			}
 		}
 	}
